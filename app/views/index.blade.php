4835177819238c7c28de0f0c44d911b02a90fe00
Carlos <bakuryuthem0@gmail.com>
2015-03-26 16:58:08 -0430

    everything was uploaded and about us was changed of position, aaaand other stuff

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,133 +4,14 @@
 <div class="row contenedorGrande banner">
 	<img src="{{ URL::to('images/banner.gif') }}">
 </div>
-<div id="project" class="row contenedorGrande">
-	<div class="container">
-		<div class="col-xs-12">
-			<div class="col-xs-6 cont_titulo_serv">
-				<img src="{{ asset('images/img_serv/nuestros servicios-01.png') }}">
-			</div>
-			<div class="col-xs-12" style="margin-bottom:2em;text-align:center;">
-				@foreach($servicios as $clave => $servicio)
-				<a href="{{ URL::to('servicios/'.$servicio->id) }}">
-					<div class="servicio" id="{{ $servicio->image }}" >
-						<img src="{{ URL::to('images/'.$servicio->image.'.png') }}">
-					</div>
-				</a>
-				@endforeach
-			</div>
-			<div class="col-xs-12">
-				<div class="col-xs-6 contenedorBot textoServicios">
-					<h3 style="text-align:left;">AGENCIA DE DISEÑO GRÁFICO PUBLICITARIO Y SISTEMAS ADMINISTRATIVOS</h3>
-					<p class="textoPromedio"><strong>Tecnographic de Venezuela, C.A.</strong>, es su agencia de Diseño Grafico y de Sistemas Administrativos, en Maracay, Venezuela; contamos con personal altamente calificado en el ramo de la publicidad digital, diseño gráfico y diseño de herramientas online como páginas web, tiendas virtuales, campañas de email, redes sociales y sistemas administrativos. Estamos ubicados en la Ciudad de Maracay, además prestamos todos nuestros servicios de diseño gráfico en el resto de Venezuela.</p>
-					<p class="textoPromedio">
-					Entre nuestros servicios como agencia publicitaria y consultora podemos citar: Asesoría en estrategias de mercadeo, Diseño de Imagen Corporativa, Diseño Gráfico, Diseño de Logotipos en Maracay, Diseño de Páginas Web  en Maracay, Diseño de Afiches en Maracay, Diseño de Banners, Diseño de Perfiles para redes sociales, Diseño  de Empaques de Productos,  Diseño de Tarjetas de Presentación, Diseño de Señalética, Fotografía de Productos, Fotografía de Modelos, Fotografía de Espacio</p>
-				</div>
-				<div class="col-xs-6 contSlider sliderPrinc">
-					<div class="mySlide">
-						<div><img src="{{URL::to('images/slides/slider1-01.png')}}"></div>
-						<div><img src="{{URL::to('images/slides/slider2-01.png')}}"></div>
-						<div><img src="{{URL::to('images/slides/slider3-01.png')}}"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div id="mini_gif">
-		<img class="separator" src="{{URL::to('images/nini-banner.gif')}}">
-	</div>
-</div>
 <div id="about" class="row contenedorGrande">
-	<div class="container">
-		<div class="col-xs-12">
-			<div class="col-xs-6 cont_titulo_about">
-				<img src="{{ asset('images/img_serv/quienes somos-01.png') }}">
-			</div>
-		</div>
-	</div>
-	<div id="banda" class="row">
-		<div class="cont_palabra col-xs-12">
-			<div id="top" class="col-xs-12">
-				<div class="col-xs-12" style="margin-top:1em;">
-					<div class="col-xs-6 medio textLeft">
-						<p class="palabra col-xs-6 textoMedio" id="c">
-							Constancia
-						</p>
-						<p  class="palabra col-xs-6 textoMedio" id="r">
-							Responsabilidad
-						</p>
-					</div>
-				</div>
-				<div class="col-xs-12" >
-					<div class="col-xs-6 medio textMid">
-						<p class="palabra col-xs-6 textoMedio" id="g">
-							Gratificación
-						</p>
-						<p class="palabra col-xs-6 textoMedio" id="i">
-							Innovación
-						</p>
-					</div>
-				</div>
-				<div class="col-xs-12">
-					<div class="col-xs-6 medio textRight">
-						<p class="palabra col-xs-6 textoMedio" id="t">
-							Tecnología
-						</p>
-						<p class="palabra col-xs-6 textoMedio" id="a">
-							Autenticidad
-						</p>
-					</div>
-				</div>
-			</div>
-			<div style="clear:both;"></div>
-			<div class="col-xs-12">
-				<div class="medio col-xs-6 textMid">
-					<h1 class="letra" id="T">T</h1>
-					<h1 class="letra" id="E">E</h1>
-					<h1 class="letra" id="C">C</h1>
-					<h1 class="letra" id="N">N</h1>
-					<h1 class="letra" id="O">O</h1>
-					<h1 class="letra" id="G">G</h1>
-					<h1 class="letra" id="R">R</h1>
-					<h1 class="letra" id="A">A</h1>
-					<h1 class="letra" id="P">P</h1>
-					<h1 class="letra" id="H">H</h1>
-					<h1 class="letra" id="I">I</h1>
-					<h1 class="letra" id="K">C</h1>
-				</div>
-			</div>
-			<div id="bot" class="col-xs-12">
-				<div class="col-xs-12">
-					<div class="col-xs-6 medio textRight">
-						<p class="palabra col-xs-4 textoMedio" id="e">
-							Evolución
-						</p>
-						<p class="palabra col-xs-4 textoMedio" id="o">
-							Organización
-						</p>
-						<p class="palabra col-xs-4 textoMedio" id="k">
-							Crecimiento
-						</p>
-					</div>
-				</div>
-				<div class="col-xs-12">
-					<div class="col-xs-6 medio textLeft">
-						<p class="palabra col-xs-4 textoMedio" id="n">
-							Negociación
-						</p>
-						<p class="palabra col-xs-4 textoMedio" id="p">
-							Profesionalismo
-						</p>
-						<p class="palabra col-xs-4 textoMedio" id="h">
-							Humildad
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	
 	<div class="container">
 		<div class="row about" style="margin-top:5em;">
+			<div class="col-xs-6 medio">
+				<h1>Quienes somos</h1>
+				<h3>Nuestro equipo</h3>
+			</div>
 			<div class="col-xs-12">
 				<div class="col-xs-6 contAbout1">
 					<div class="about1 ab2">
@@ -154,10 +35,52 @@
 			</div>
 		</div>
 	</div>
-	<div class="refran">
-		<img src="{{URL::to('images/refran-01.png')}}">
+	
+</div>
+<div class="row contenedorGrande">
+	<div id="project" class="row contenedorGrande">
+		<div class="container">
+			<div class="col-xs-12">
+				<div class="col-xs-6 cont_titulo_serv">
+					<img src="{{ asset('images/img_serv/nuestros servicios-01.png') }}">
+				</div>
+				<div class="col-xs-12" style="margin-bottom:2em;text-align:center;">
+					@foreach($servicios as $clave => $servicio)
+					<a href="{{ URL::to('servicios/'.$servicio->id) }}">
+						<div class="servicio" id="{{ $servicio->image }}" >
+							<img src="{{ URL::to('images/'.$servicio->image.'.png') }}">
+						</div>
+					</a>
+					@endforeach
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	<div id="about" class="row contenedorGrande">
+		<div class="container">
+			<div class="col-xs-12">
+				<h1 style="text-align:left;">AGENCIA DE DISEÑO GRÁFICO PUBLICITARIO Y SISTEMAS ADMINISTRATIVOS</h1>
+				<p class="textoPromedio"><strong>Tecnographic de Venezuela, C.A.</strong>, es su agencia de Diseño Grafico y de Sistemas Administrativos, en Maracay, Venezuela; contamos con personal altamente calificado en el ramo de la publicidad digital, diseño gráfico y diseño de herramientas online como páginas web, tiendas virtuales, campañas de email, redes sociales y sistemas administrativos. Estamos ubicados en la Ciudad de Maracay, además prestamos todos nuestros servicios de diseño gráfico en el resto de Venezuela.</p>
+				<p class="textoPromedio">
+				Entre nuestros servicios como agencia publicitaria y consultora podemos citar: Asesoría en estrategias de mercadeo, Diseño de Imagen Corporativa, Diseño Gráfico, Diseño de Logotipos en Maracay, Diseño de Páginas Web  en Maracay, Diseño de Afiches en Maracay, Diseño de Banners, Diseño de Perfiles para redes sociales, Diseño  de Empaques de Productos,  Diseño de Tarjetas de Presentación, Diseño de Señalética, Fotografía de Productos, Fotografía de Modelos, Fotografía de Espacio</p>
+			</div>
+			<div class="col-xs-6 contSlider sliderPrinc">
+				<div class="mySlide">
+					<div><img src="{{URL::to('images/slides/slider1-01.png')}}"></div>
+					<div><img src="{{URL::to('images/slides/slider2-01.png')}}"></div>
+					<div><img src="{{URL::to('images/slides/slider3-01.png')}}"></div>
+				</div>
+			</div>
+			<div class="col-xs-12 contenedorBot textoServicios">
+				<p class="textoPromedio"><strong>Tecnographic de Venezuela, C.A.</strong>, es su agencia de Diseño Grafico y de Sistemas Administrativos, en Maracay, Venezuela; contamos con personal altamente calificado en el ramo de la publicidad digital, diseño gráfico y diseño de herramientas online como páginas web, tiendas virtuales, campañas de email, redes sociales y sistemas administrativos. Estamos ubicados en la Ciudad de Maracay, además prestamos todos nuestros servicios de diseño gráfico en el resto de Venezuela.</p>
+				<p class="textoPromedio">
+				Entre nuestros servicios como agencia publicitaria y consultora podemos citar: Asesoría en estrategias de mercadeo, Diseño de Imagen Corporativa, Diseño Gráfico, Diseño de Logotipos en Maracay, Diseño de Páginas Web  en Maracay, Diseño de Afiches en Maracay, Diseño de Banners, Diseño de Perfiles para redes sociales, Diseño  de Empaques de Productos,  Diseño de Tarjetas de Presentación, Diseño de Señalética, Fotografía de Productos, Fotografía de Modelos, Fotografía de Espacio</p>
+			</div>
+		</div>
 	</div>
 </div>
+
 <div class="modal fade" id="showImg" tabindex="-1" role="dialog" aria-labelledby="showImg" aria-hidden="true">
 		<div class="modal-dialog imgLiderUp">
 			<div class="modal-content">
